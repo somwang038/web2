889f5326f77d99cf99887a31984dece4a04927b4
somwang038 <somwang038@hotmail.com>
2014-03-03 23:49:48 +0700

    ทดลองใช้ Bootstrap

---
+++ header.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+<link href="bootstrap/css/carousel.css" rel="stylesheet">
+<link rel="shortcut icon" href="bootstrap/ico/favicon.ico">
+<style id="holderjs-style" type="text/css"></style>
+
+<!--<script src="bootstrap/js/jquery-1.11.0.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>-->
